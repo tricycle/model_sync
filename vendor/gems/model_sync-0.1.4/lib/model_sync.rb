9b7d2d21eb67702f779697918a5423bc6ecd0190
Wesley Moore <wes@trike.com.au>
2010-05-07 16:11:10 +1000

    Set slave id to master id in model_sync
    Instead of the other way around.

--- vendor/gems/model_sync-0.1.4/lib/model_sync.rb
+++ vendor/gems/model_sync-0.1.4/lib/model_sync.rb
@@ -38,9 +38,8 @@ module ModelSync
           # If we've received a create_{slave_model} call then create a new instance of it and sync to it
           new_instance = self.class.slave_model_class.new
           perform_sync(new_instance)
-          # Save the new instance so that its primary key is generated and pass this value onto our master model
+          new_instance.id = self.id
           new_instance.save
-          self.update_attribute(self.class.relationship.keys.first, new_instance.read_attribute(self.class.relationship.values.first.to_s))
         end
       when /^synch?ed_with_#{self.class.slave_model_name}\?$/
         !!find_slave_instance
